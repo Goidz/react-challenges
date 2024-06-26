c76b6faf2ada9ff4c31c8ad152620756a99968a0
Gideon Horn <gideon.h99@gmail.com>
2024-06-26 08:02:14 +0000

    Add: Navbar

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,20 @@
 import css from './App.module.css';
 import Sidebar from "./components/Sidebar.js";
+import NavBarSimple from "./components/NavBarSimple.js";
 
 function App() {
   return (
     <div className={css.App}>
-      {<Sidebar />}
+      <NavBarSimple />
+      <Sidebar />
     </div>
   );
 }
 
 export default App;
+
+// nvm install 16 && nvm use 16
+//cd my-app
+//nvm install 16.18.0
+//nvm use 16.18.0
+//npm start
