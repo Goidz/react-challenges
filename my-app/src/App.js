a8b325aa3ece3dc63b16681305a31e52810ccf03
Gideon Horn <gideon.h99@gmail.com>
2024-06-27 13:14:39 +0000

    Add: login button, login form My gallery h1

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,13 @@
-import css from './App.module.css';
-import Sidebar from "./components/Sidebar.js";
-import NavBarSimple from "./components/NavBarSimple.js";
+import css from "./App.module.css";
+// import NavBarSimple from "./components/NavBarSimple";
+import Sidebar from "./components/Sidebar";
+import NavBarForm from "./components/NavBarForm";
 
 function App() {
   return (
     <div className={css.App}>
-      <NavBarSimple />
+      {/* Add your components here */}
+      <NavBarForm />
       <Sidebar />
     </div>
   );
@@ -13,7 +15,7 @@ function App() {
 
 export default App;
 
-// nvm install 16 && nvm use 16
+
 //cd my-app
 //nvm install 16.18.0
 //nvm use 16.18.0
